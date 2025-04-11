f8f79a93c3969fc6fcb13d073cd7a2a71e7a7d69
konohat3 <konohat3@gmail.com>
2025-04-11 21:29:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def omikuji_result():
     return render_template('result.html', top=top, bottom=bottom)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
